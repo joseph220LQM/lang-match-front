d355f3e4fdc7a79a27ed677aa48807a2c3036ad7
jose <3225253086jl@gmail.com>
2025-10-18 09:31:32 -0500

    verificacionec2

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const BASE = "https://d3hem1c2ka17ud.cloudfront.net/api";
+const BASE = "http://ec2-3-220-24-160.compute-1.amazonaws.com";
 
 /**
  * Crea una nueva sala (sesión de práctica)
